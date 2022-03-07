c51edfe48a96d623d43f7733db93690166dec988
eduardojmrosa <eduardojose222002@hotmail.com>
2022-03-07 20:56:25 -0300

    calculadora com DOM

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-let personSelectOperation= Number(prompt("Selecione uma operação\n1-Soma\n2-Subtração\n3-Multiplicação\n4-Divisão\n5-Potenciação"));
-let value1 = Number(prompt("Insira o primeiro valor:"))
-let value2 = Number(prompt("Insira o segundo valor:"))
-let result = 0
+function insert(num){
+ var numero = document.getElementById('resultado').innerHTML;
+ document.getElementById('resultado').innerHTML = numero+num;
 
-switch(personSelectOperation) 
-{ case 1:
-    result = value1+value2;
-    console.log(result);
-    break;
-    case 2:
-    result = value1-value2;
-    console.log(result)
-    break;
-    case 3:
-    result = value1*value2;
-    console.log(result)
-    break;
-    case 4:
-    result = value1/value2;
-    console.log(result)
-    break;
-    case 5:
-    result = value1**value2;
-    console.log(result)
-    break;
+}
+function clean(){
+    document.getElementById('resultado').innerHTML ="";
+}
+function back(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
+function calcular(){
+    var resultado = document.getElementById('resultado').innerHTML;
+
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+
 }
